475fb09a1d271204e1ccec932cb0384c37a38bda
Konrad <konrad.kusiak.17@aberdeen.ac.uk>
2020-05-21 12:16:14 +0200

    Final version

--- crossword/generate.py
+++ crossword/generate.py
@@ -291,14 +291,14 @@ class CrosswordCreator():
 
 def main():
 
-    '''# Check usage
+    # Check usage
     if len(sys.argv) not in [3, 4]:
-        sys.exit("Usage: python generate.py structure words [output]")'''
+        sys.exit("Usage: python generate.py structure words [output]")
 
     # Parse command-line arguments
-    structure = 'data/structure2.txt'#sys.argv[1]
-    words =  'data/words0.txt'#sys.argv[2]
-    output = 'output.png'#sys.argv[3] if len(sys.argv) == 4 else None
+    structure = sys.argv[1]
+    words =  sys.argv[2]
+    output = sys.argv[3] if len(sys.argv) == 4 else None
 
     # Generate crossword
     crossword = Crossword(structure, words)
