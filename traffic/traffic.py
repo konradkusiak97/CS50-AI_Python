9b944d3aa1032fed9ed80c685ed4476e2aaa0c43
Konrad <konrad.kusiak.17@aberdeen.ac.uk>
2020-06-02 18:57:12 +0200

    Implemented get_model but model is not training properly, accuracy: 8%

--- traffic/traffic.py
+++ traffic/traffic.py
@@ -61,7 +61,7 @@ def load_data(data_dir):
     images = []
     labels = []
     categories = os.listdir(data_dir)
-
+    categories.remove('.DS_Store')
     for c in categories:
         categoryLink = os.path.join(data_dir, c)
         photos = os.listdir(categoryLink)
@@ -84,7 +84,37 @@ def get_model():
     `input_shape` of the first layer is `(IMG_WIDTH, IMG_HEIGHT, 3)`.
     The output layer should have `NUM_CATEGORIES` units, one for each category.
     """
-    
+    # Create a convolutional neural network
+    model = tf.keras.models.Sequential([
+
+        # Convolutional layer. Learn 32 filters using a 3x3 kernel
+        tf.keras.layers.Conv2D(
+            32, (3, 3), activation="relu", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
+        ),
+
+        # Max-pooling layer, using 2x2 pool size
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+
+        # Flatten units
+        tf.keras.layers.Flatten(),
+
+        # Add a hidden layer with dropout
+        tf.keras.layers.Dense(128, activation="relu"),
+        tf.keras.layers.Dropout(0.5),
+
+        # Add an output layer with output units for all 10 digits
+        tf.keras.layers.Dense(NUM_CATEGORIES, activation="softmax")
+    ])
+
+    # Train neural network
+    model.compile(
+        optimizer="adam",
+        loss="categorical_crossentropy",
+        metrics=["accuracy"]
+    )
+
+    return model
+
 
 if __name__ == "__main__":
     main()
