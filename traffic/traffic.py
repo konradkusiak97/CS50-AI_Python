5f37d24a48f0014ab34bc80408627f5c4272eca7
Konrad <konrad.kusiak.17@aberdeen.ac.uk>
2020-06-02 22:23:35 +0200

    Accuracy 0.94 with one hidden layer

--- traffic/traffic.py
+++ traffic/traffic.py
@@ -89,17 +89,18 @@ def get_model():
 
         # Convolutional layer. Learn 32 filters using a 3x3 kernel
         tf.keras.layers.Conv2D(
-            32, (3, 3), activation="relu", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
+            100, (3, 3), activation="relu", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
         ),
 
         # Max-pooling layer, using 2x2 pool size
-        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+        tf.keras.layers.MaxPooling2D(pool_size=(3, 3)),
 
         # Flatten units
         tf.keras.layers.Flatten(),
 
         # Add a hidden layer with dropout
-        tf.keras.layers.Dense(128, activation="relu"),
+        tf.keras.layers.Dense(300, activation="relu"),
+
         tf.keras.layers.Dropout(0.5),
 
         # Add an output layer with output units for all 10 digits
